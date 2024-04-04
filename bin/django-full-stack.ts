68ba7142ec35f79e2a292dab6353480cd69e1dc7
wtah <thilo.hofmeister@ai-consulting.solutions>
2024-04-04 17:23:47 +0200

    initial cicd push

--- bin/django-full-stack.ts
+++ bin/django-full-stack.ts
@@ -1,30 +1,32 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { AwsSolutionsChecks } from 'cdk-nag'
-import { Aspects } from 'aws-cdk-lib';
-import { BuildConfig, getConfig } from '../lib/get-config';
-import { DjangoStack } from '../lib/django-stack';
+import {AwsSolutionsChecks} from 'cdk-nag'
+import {Aspects} from 'aws-cdk-lib';
+import {BuildConfig, getConfig} from '../lib/get-config';
+import {DjangoStack} from '../lib/django-stack';
 import {TrustStack} from "../lib/constructs/trust-stack";
 
 const app = new cdk.App();
 
 let buildConfig: BuildConfig = getConfig();
 
-
+// Retrieve the prefix from the context parameters
+let prefix = app.node.tryGetContext('prefix') ?? 'demo';
+const environment = prefix == 'prod' ? 'prod' : 'dev';
 
 const accountEnv =
     {
-      region: buildConfig.Parameters.REGION,
-      account: buildConfig.Parameters.ACCOUNT_ID,
+        region: buildConfig.Parameters.REGION,
+        account: buildConfig.Parameters.ACCOUNT_ID,
     };
 
-new TrustStack(app, "TrustStack",{
-  env: accountEnv
+new TrustStack(app, "TrustStack", {
+    env: accountEnv,
 }, buildConfig);
 
-new DjangoStack(app, 'DjangoStack', {
-  env: accountEnv
+new DjangoStack(app, `${prefix}-DjangoStack`, {
+    env: accountEnv,
 }, buildConfig);
 
-Aspects.of(app).add(new AwsSolutionsChecks({ verbose: true }))
+Aspects.of(app).add(new AwsSolutionsChecks({verbose: true}))
